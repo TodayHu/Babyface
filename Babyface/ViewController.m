c8a3bfc9f8173514d0617e8970cf0ff3a91bae58
Vinh Phuc Dinh <dinh@mocava.de>
2015-01-10 18:02:33 +0100

    Add detection of sound

--- Babyface/ViewController.m
+++ Babyface/ViewController.m
@@ -6,11 +6,14 @@
 //  Copyright (c) 2015 Marco Klingmann. All rights reserved.
 //
 
+#import <AVFoundation/AVFoundation.h>
 #import "ViewController.h"
 #import "MMWormhole.h"
 
 @interface ViewController () {
     MMWormhole *_wormhole;
+    AVAudioRecorder *_audioRecorder;
+    NSTimer *_timer;
 }
 
 @end
@@ -22,31 +25,41 @@
     
     _wormhole = [[MMWormhole alloc] initWithApplicationGroupIdentifier:@"group.com.mocava.watchkit.babyface.sharedcontainer" optionalDirectory:nil];
     
-    
+    NSURL *url = [NSURL fileURLWithPath:@"/dev/null"];
+    NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:
+                              [NSNumber numberWithFloat: 44100.0],                 AVSampleRateKey,
+                              [NSNumber numberWithInt: kAudioFormatAppleLossless], AVFormatIDKey,
+                              [NSNumber numberWithInt: 1],                         AVNumberOfChannelsKey,
+                              [NSNumber numberWithInt: AVAudioQualityMax],         AVEncoderAudioQualityKey,
+                              nil];
+    _audioRecorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:nil];
+    _audioRecorder.meteringEnabled = YES;
+    [_audioRecorder prepareToRecord];
+    [_audioRecorder record];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(listenToBaby) userInfo:nil repeats:YES];
+    [_timer fire];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
     UIImage *image = [UIImage imageNamed:@"baby-red"];
-    NSString *msg = [NSString stringWithFormat:@"Image is %@", image];
-    UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Image" message:msg delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-    [av show];
     NSData *imageData = UIImagePNGRepresentation(image);
     
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [_wormhole passMessageObject:imageData identifier:@"UpdateImage"];
     });
-    
-    
-    
-    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)listenToBaby {
+    [_audioRecorder updateMeters];
+    CGFloat power = [_audioRecorder averagePowerForChannel:0];
+    if (power>=-20.0) {
+        NSLog(@"Baby is crying...");
+    } else {
+        NSLog(@"Baby is fine.");
+    }
 }
 
 @end
