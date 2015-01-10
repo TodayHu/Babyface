882b760c30c99bf02b47eb03d7d38bee832d609d
Marco Klingmann <marco.klingmann@taktil.ch>
2015-01-10 14:08:59 +0100

    Initial Commit

---
+++ Babyface/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Babyface
+//
+//  Created by Marco Klingmann on 10.01.15.
+//  Copyright (c) 2015 Marco Klingmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
